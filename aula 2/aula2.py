2352a3f1c058822575576b60f7112512cd4dc12c
348181595men <glfjg@hotmail.com>
2024-12-02 10:00:49 -0300

    aprendi o que é uma variavel

---
+++ aula 2/aula2.py
@@ -0,0 +1,11 @@
+print('bom dia bigode')
+
+
+nome_usuario = 'guilherme$' #tipo -> str = string
+idade_usuario = 17 #tipo -> int = integer
+altura_usuario = 1.84 #tipo -> float
+maior_de_idade = False #tipo -> bool = boolean 
+
+print('bom dia bigode', nome_usuario, 'sua idade é', idade_usuario, 'sua altura é', altura_usuario, 'voce é maior de idade?', maior_de_idade)
+
+
